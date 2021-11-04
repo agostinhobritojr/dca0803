f41873e7820b1748016bed69f457a05279d07ae7
Agostinho Brito <agostinhobritojr@gmail.com>
2021-11-04 16:04:16 -0300

    teste

--- matrizeseficiente/main.c
+++ matrizeseficiente/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> // memcpy
-
+#include <string.h> // memcpy()
+#include <time.h> // clock()
 int main(){
   int **x;
   int nl, nc;
-  int i, j;
+  int i, j, k;
   int **y;
+  clock_t t;
 
   nl = 3; nc = 4;
 
@@ -29,19 +30,46 @@ int main(){
   for(i=1; i<nl; i++){
     y[i] = y[i-1] + nc;
   }
-  // copia com loop aninhando
+
   for(i=0; i<nl; i++){
     for(j=0; j<nc; j++){
       x[i][j] = i+j;
     }
   }
-  // copia com loop unico
-  for(i=0; i<nl*nc-1; i++){
-    y[0][i] = x[0][i];
-  }
-  // copia com memcpy
-  memcpy(y[0],x[0], nl*nc*sizeof (int));
 
+  // copia com loop aninhado
+  t = clock();
+  for(k=0; k<500000; k++){
+    for(i=0; i<nl; i++){
+      for(j=0; j<nc; j++){
+        y[i][j] = x[i][j];
+      }
+    }
+  }
+  t = clock() - t;
+  printf ("levou %f seconds.\n",
+          ((float)t)/CLOCKS_PER_SEC);
+
+
+  // copia com loop unico
+  t = clock();
+  for(k=0; k<500000; k++){
+    for(i=0; i<nl*nc-1; i++){
+      y[0][i] = x[0][i];
+    }
+  }
+  t = clock() - t;
+  printf ("levou %f seconds.\n",
+          ((float)t)/CLOCKS_PER_SEC);
+
+  // copia com memcpy
+  t = clock();
+  for(k=0; k<500000; k++){
+    memcpy(y[0],x[0], nl*nc*sizeof (int));
+  }
+  t = clock() - t;
+  printf ("levou %f seconds.\n",
+          ((float)t)/CLOCKS_PER_SEC);
 
   for(i=0; i<nl; i++){
     for(j=0; j<nc; j++){
@@ -51,7 +79,7 @@ int main(){
   }
   printf("\n");
 
- // x[2][-3]=-1;
+  // x[2][-3]=-1;
 
   for(i=0; i<nl; i++){
     for(j=0; j<nc; j++){
@@ -59,8 +87,13 @@ int main(){
     }
     printf("\n");
   }
+
   free(x[0]);
   free(x);
+
+  free(y[0]);
+  free(y);
+
 
   return 0;
 }
