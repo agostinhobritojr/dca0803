690c1534890561c2134f8f10e1eee4dac0818284
Agostinho Brito <agostinhobritojr@gmail.com>
2022-04-26 16:21:29 -0300

    matrizes

--- matrizesc/main.c
+++ matrizesc/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main(){
-  int **x;
+  int **x, **y;
   int nl=3, nc=4, i, j;
   // aloca vetor auxiliar
   x = malloc(nl * sizeof (int*));
@@ -10,14 +10,23 @@ int main(){
   for(i=0; i<nl; i++){
     x[i] = malloc(nc * sizeof(int));
   }
+
+  y = malloc(nl * sizeof (int*));
+  // aloca cada uma das linhas
+  for(i=0; i<nl; i++){
+    y[i] = malloc(nc * sizeof(int));
+  }
   for(i=0; i<nl; i++){
     for(j=0; j<nc; j++){
       x[i][j] = rand()%99;
     }
   }
   for(i=0; i<nl; i++){
+    memcpy(y[i], x[i], nc*sizeof(int));
+  }
+  for(i=0; i<nl; i++){
     for(j=0; j<nc; j++){
-      printf("%2d ", x[i][j]);
+      printf("%2d ", y[i][j]);
     }
     printf("\n");
   }
