83f820cc93e166d5a999e21347b2e11b8b58a98b
Agostinho Brito <agostinhobritojr@gmail.com>
2022-08-30 15:39:46 -0300

    .

--- hello/main.c
+++ hello/main.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
-int z; // globais
-
-void troca(int a, int b){
-  int jose;
+void troca(int *a, int *b){
   int tmp;
-  printf("a = %d; b = %d\n", a, b);
-  tmp=a; a=b; b=tmp;
-  printf("a = %d; b = %d\n", a, b);
+  printf("*a = %d; *b = %d\n", *a, *b);
+
+  tmp=*a; // copia para tmp o conteudo de a
+
+  *a=*b; // conteudo de a recebe conteudo de b
+
+  *b=tmp;
+
+  printf("*a = %d; *b = %d\n", *a, *b);
 }
 int main(void){
   int x=3, y=4;
   printf("x = %d; y = %d\n", x, y);
-  troca(x, y);
+  troca(&x, &y);
   printf("x = %d; y = %d\n", x, y);
   return 0;
 }
--- ponteiros/main.c
+++ ponteiros/main.c
@@ -7,4 +7,9 @@ int main(void){
   y = *px;
 
   printf("*px = %d\n", *px);
+  printf("px = %p\n", px);
+  printf("&x = %p\n", &x);  
+  printf("&y = %p\n", &y);
+  printf("&px = %p\n", &px);  
+
 }
