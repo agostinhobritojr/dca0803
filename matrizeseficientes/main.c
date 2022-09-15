7874138b6b9f95d76cc87b50e4342bc1690814c7
Agostinho Brito <agostinhobritojr@gmail.com>
2022-09-15 16:08:44 -0300

    matrizeficiente

--- matrizeseficientes/main.c
+++ matrizeseficientes/main.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> // para memcpy()
 
 int main(){
   int **x, nl=4, nc=3, i, j;
+  int **y;
 
   // aloca o array auxiliar
   x = (int**) malloc(nl*sizeof(int*));
+  y = (int**) malloc(nl*sizeof(int*));
   // aloca a matriz e guarda o endereco retornado
   // no local que referencia a primeira linha
   // na matriz x
   x[0] = (int*) malloc(nl*nc*sizeof(int));
+  y[0] = (int*) malloc(nl*nc*sizeof(int));
 
   // fixacao dos ponteiros para os inicios
   // de cada linha
   for(i=1; i<nl; i++){
     x[i] = x[i-1] + nc;
+  }
+  for(i=1; i<nl; i++){
+    y[i] = y[i-1] + nc;
   }
 
   for(i=0; i<nl; i++){
@@ -22,10 +29,18 @@ int main(){
       x[i][j] = rand()%10;
     }
   }
+  for(i=0; i<nl; i++){
+    for(j=0; j<nc; j++){
+      y[i][j] = x[i][j];
+    }
+  }
+
+//  memcpy(destino, origem, numbytes);
+//  memcpy(y[0],x[0],nl*nc*sizeof(int));
 
   for(i=0; i<nl; i++){
     for(j=0; j<nc; j++){
-      printf("%2d ", x[i][j]);
+      printf("%2d ", y[i][j]);
     }
     printf("\n");
   }
@@ -39,6 +54,9 @@ int main(){
   printf("passou!\n");
   free(x[0]);
   free(x);
+
+  free(y[0]);
+  free(y);
 
   return 0;
 }
