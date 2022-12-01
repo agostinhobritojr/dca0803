824f2f8bbb4a8d9d4290cd23c1a47d233656fd69
Agostinho Brito <agostinhobritojr@gmail.com>
2022-12-01 15:44:45 -0300

    gabaritos

---
+++ gabaritos-classes/alo.h
@@ -0,0 +1,24 @@
+#ifndef ALO_H
+#define ALO_H
+
+template <typename T>
+class Alo{
+private:
+  T x, y;
+public:
+  Alo(T param);
+  T getX();
+};
+
+template <typename T>
+Alo<T>::Alo(T param){
+  x = param;
+}
+
+template <typename T>
+T Alo<T>::getX(){
+  return x;
+}
+
+
+#endif // ALO_H
