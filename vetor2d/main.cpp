d565bcd828c17cb7f76a2eb641518074a90422ec
Agostinho <agostinhobritojr@gmail.com>
2023-05-06 06:24:10 -0300

    vetor2d

--- vetor2d/main.cpp
+++ vetor2d/main.cpp
@@ -12,6 +12,11 @@ public:
     // qualquer trecho de codigo pode usar o
     // que vem a seguir
     void setX(float x_){
+        // so GOSTARIA de permitir valores
+        // maiores ou iguais que zero
+        if(x_ < 0){
+            return;
+        }
         x = x_;
     }
     float getX(){
@@ -19,12 +24,17 @@ public:
     }
 }; // <--- NAO ESQUECA DO ";"
 
+void setX(float x_){
+    std::cout << "Alo, maria\n";
+}
+
 int main(void){
     // Declaracao do objeto v
     // que pertence aa classe Vetor2d
     Vetor2d v;
    // v.x = 3;
     v.setX(3);
+    setX(3);
     std::cout << v.getX() << "\n";
 }
 
