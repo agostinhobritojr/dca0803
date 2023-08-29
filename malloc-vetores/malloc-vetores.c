6425dd925a1f0d30ceb92f668cb2246731782c81
Agostinho <agostinhobritojr@gmail.com>
2023-08-29 16:27:40 -0300

    .

--- malloc-vetores/malloc-vetores.c
+++ malloc-vetores/malloc-vetores.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 
 int main(void){
-  int *x, i, n;
+  int *x, *y, i, n;
   n = 10;
   printf("x  = %p\n", x);
   printf("&x = %p\n", &x);
   printf("&i = %p\n", &i);
   printf("&n = %p\n", &n);
   x = (int*) malloc(n * sizeof(int));
+  y = (int*) malloc(n * sizeof(int));
   printf("x  = %p\n", x);
   //   ^^^^ casting
   x[3] = 4;
@@ -18,10 +19,19 @@ int main(void){
     x[i] = i;
   }
   for(i=0; i<n; i++){
-    printf("%d, ", x[i]);
+    y[i] = x[i];
+  }
+  for(i=0; i<n; i++){
+    printf("%d, ", y[i]);
+  }
+  printf("\n");
+  x[4] = 20;
+  for(i=0; i<n; i++){
+    printf("%d, ", y[i]);
   }
   printf("\n");
   free(x);
+  free(y);
   /* nao pode usar memoria jah liberada
   for(i=0; i<20; i++){
     printf("%d, ", x[i]);
