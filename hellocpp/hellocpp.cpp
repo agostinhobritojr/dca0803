e8d3fc455b0f502c78ba0fb71d790717336c560c
Agostinho <agostinhobritojr@gmail.com>
2026-04-14 16:18:08 -0300

    .

---
+++ hellocpp/hellocpp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(void) {
+  float x = 5;
+  std::cout << "x = " << x << "\n";
+  std::cout << "continua...\n";
+  std::cout << "Digite x: ";
+  std::cin >> x;
+  std::cout << "x = " << x << "\n";
+  return 0;
+}
